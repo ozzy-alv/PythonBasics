257cbd26a13c5f15069e361ea0851aba5b3a7c12
Ozzy <103617407+ozzy-alv@users.noreply.github.com>
2025-02-01 21:25:49 -0500

    Textbook exercises

---
+++ Basics_ch2/names_cases.py
@@ -0,0 +1,21 @@
+#Textbook exercises
+
+#Personal Message
+friend = "Joe"
+print("Hey are you my friend " + friend + "?")
+
+#Making name all uppercase
+print(friend.upper())
+
+#Print a famous quote
+quote = '"I think, therefore I am." '
+print(quote)
+
+#Creating famous person
+famous_person = "Rene Descartes "
+
+#Adding the famous quote to the famous name
+print(famous_person + "once said, " + quote)
+
+#Now removing all the white spaces for chaos!
+print(famous_person.strip() + "once said, " + quote)
