b0e4a6ebf372b47dcb2acaca6f7ee6b97aeb0861
Ozzy <103617407+ozzy-alv@users.noreply.github.com>
2025-02-03 21:09:06 -0500

    Intro to lists

---
+++ Basics_ch3/bicycles.py
@@ -0,0 +1,16 @@
+#Introduction to lists
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized'] #list
+print(bicycles)
+
+#accessing an item in the list
+
+print(bicycles[0]) #prints trek
+
+#print with title name
+print(bicycles[2].title()) #prints Redline
+
+#special character to access last item in list
+print(bicycles[-1]) #prints specialized
+
+#magic trick algorithm could be a good project. In which we follow a magic trick and convert it to a program.
