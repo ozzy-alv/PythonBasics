d8230e61d7f449c21d341db38b97475e2f5e8859
Ozzy <103617407+ozzy-alv@users.noreply.github.com>
2025-02-03 21:21:25 -0500

    Practicing lists

--- Basics_ch3/practice_lists.py
+++ Basics_ch3/practice_lists.py
@@ -1,1 +1,16 @@
 #exercises to get comfortable with creating and implementing basic lists
+
+#exercise, create a list of your friends names
+
+friends = ['Bryam', 'Bella', 'Coco', 'Kiki']
+
+print(friends[0])
+print(friends[1])
+print(friends[2])
+print(friends[3])
+
+#now with the list of friends print a message to them, each message should be the same
+
+message = "Welcome, " + friends[0] + " everyone has been dying to meet you. Please make yourself at home."
+
+print(message)
