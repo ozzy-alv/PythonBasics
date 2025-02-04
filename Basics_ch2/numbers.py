b3a2b31f97c28f973aa1abfa7fbb5b08b657440e
Ozzy <103617407+ozzy-alv@users.noreply.github.com>
2025-02-03 20:45:58 -0500

    completing a set of exercises involving numbers

---
+++ Basics_ch2/numbers.py
@@ -0,0 +1,20 @@
+#exercise create four lines that result in the number eight
+
+#Addition
+print(4 + 4)
+
+#Subtraction
+print(16 - 8)
+
+#Multiplication
+print(4 * 2)
+
+#Division
+print(48 // 6)
+
+#exercise, store my favorite number then create a message with that number
+
+favorite_number = 7
+
+message = str(favorite_number) + " is the number of luck"
+print(message)
