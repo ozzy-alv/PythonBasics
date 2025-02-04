98b4e2c7f277cf742da4a839c1be8c74100d1197
Ozzy <103617407+ozzy-alv@users.noreply.github.com>
2025-02-03 20:45:02 -0500

    adding comment, turning type of integer to a string through object manipulation

--- Basics_ch2/hbd.py
+++ Basics_ch2/hbd.py
@@ -1,4 +1,6 @@
-age = 23
-message = "Happy " + str(age) + "rd Birthday!"
+#Manipulating type of, turning an integer into a string
+
+age = 23 #variable of type integer
+message = "Happy " + str(age) + "rd Birthday!" #convert to string with str() function
 
 print(message)
