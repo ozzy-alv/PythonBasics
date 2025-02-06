58fd9fc906ca176c896644692ce8828f31967317
Ozzy <103617407+ozzy-alv@users.noreply.github.com>
2025-02-05 20:37:58 -0500

    Practicing Loops

--- Basics_ch4/practice_loops_one.py
+++ Basics_ch4/practice_loops_one.py
@@ -2,5 +2,18 @@
 
 pizzas = ['margarita', 'pepperoni', 'supreme']
 
+#iterating over each pizza and printing a phrase including each name.
 for pizza in pizzas:
     print(f"{pizza.title()} pizza is great! \n")
+
+#now think of three different animals in a list store these values and use for loop to print out each name.
+
+animals = ["cat", "lion", "tiger"]
+
+#iterating and printing each animal
+for animal in animals:
+    
+    #then print a statement about each animal
+    print(f"A {animal.title()} is very dangerous but extremely cute. \n")    
+
+print("Fun fact, these animals are all felines!")    
