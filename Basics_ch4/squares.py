02b216058e447eb8b3101bfdf0e2889a15683cd1
Ozzy <103617407+ozzy-alv@users.noreply.github.com>
2025-02-05 21:40:02 -0500

    Learning to create lists by looping

--- Basics_ch4/squares.py
+++ Basics_ch4/squares.py
@@ -19,4 +19,19 @@ for n in range(1, 11):
     squares.append(n**2) #directly adding the squared numbers to the list
 
 #printing the list    
-print(squares)    
+print(squares)
+
+#printing a total of numbers in list    
+total = sum(squares)
+print(total)
+
+#introduction to list comprehension used to create lists in one line of code
+
+squares = [n**2 for n in range(1, 11)] #another concise way of generating a list, in this case of squared numbers
+print(squares)
+
+#trying it again
+
+numbers = [n for n in range(1, 11)]
+print(numbers)
+
