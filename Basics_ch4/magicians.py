7a0a6009fb150ec3458cde3baab06402bd0a5d9d
Ozzy <103617407+ozzy-alv@users.noreply.github.com>
2025-02-05 20:20:27 -0500

    learning loops

---
+++ Basics_ch4/magicians.py
@@ -0,0 +1,9 @@
+#creating magicians
+magicians = ['houdini', 'blaine', 'angel']
+
+#for every magician in the list of magicians greet them.
+for magician in magicians:
+    print(f"Hello {magician.title()} that was a great trick!")
+    print(f"{magician.title()}, I can't wait to see your next trick! \n")
+
+print("Thank you, everyone, that was an amazing magic show!")    
