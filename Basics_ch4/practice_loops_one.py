5b33f46499f1357646b449ea6d14baeb5460d8b2
Ozzy <103617407+ozzy-alv@users.noreply.github.com>
2025-02-05 20:36:57 -0500

    Practicing loops

---
+++ Basics_ch4/practice_loops_one.py
@@ -0,0 +1,6 @@
+#create a list of three favorite types of pizza store these values and iterate them each, print each pizza
+
+pizzas = ['margarita', 'pepperoni', 'supreme']
+
+for pizza in pizzas:
+    print(f"{pizza.title()} pizza is great! \n")
