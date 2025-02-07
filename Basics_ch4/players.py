b9f5c5dbf2e094dd1c370cc27d104235eb3e11a4
Ozzy <103617407+ozzy-alv@users.noreply.github.com>
2025-02-06 19:26:39 -0500

    slicing

--- Basics_ch4/players.py
+++ Basics_ch4/players.py
@@ -7,3 +7,15 @@ players = ['Charlie', 'Mindy', 'Cindy', 'Leo', 'Tom']
 #to slice you declare the array and insert values brackets separated by a colon
 print(players[0:3])
 
+#you can also generate any subset in anyplace within the array
+print(players[1:3])
+
+#if you omit the first value of slice you automatically start from the beginning
+print(players[:4])
+
+#if you omit the second value then you start from the value and onward
+print(players[3:])
+
+#negative values print starting from the end of the list
+print(players[-3:])
+
