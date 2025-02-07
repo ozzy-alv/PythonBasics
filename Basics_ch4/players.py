0b93b14e9f64164d09f3bd3f1c87fa37afc234ff
Ozzy <103617407+ozzy-alv@users.noreply.github.com>
2025-02-06 19:19:26 -0500

    Slicing

---
+++ Basics_ch4/players.py
@@ -0,0 +1,9 @@
+#slicing a list
+
+#to slice a list you declare the idx you want to start at and then choose the index you want to end plus another index.
+
+players = ['Charlie', 'Mindy', 'Cindy', 'Leo', 'Tom']
+
+#to slice you declare the array and insert values brackets separated by a colon
+print(players[0:3])
+
