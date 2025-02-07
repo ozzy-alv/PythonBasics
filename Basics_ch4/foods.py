d0a79ee1daba3ae9dfae2654940c1ccc503ee2ed
Ozzy <103617407+ozzy-alv@users.noreply.github.com>
2025-02-06 19:36:51 -0500

    copying list with slice

---
+++ Basics_ch4/foods.py
@@ -0,0 +1,11 @@
+#copying lists
+
+#create a list of favorite foods and copy it
+foods = ['pizza', 'burger', 'lo mein', 'sushi', 'steak']
+
+#to copy just set the slice with no values to a variable
+friend_food = foods[:]
+
+#print to confirm
+print(foods)
+print(friend_food)
